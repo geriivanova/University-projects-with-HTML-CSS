35d7c8b54868eb810bda060a3b7bc42edc6161c1
Gergana Ivanova <geri7423@abv.bg>
2025-01-05 14:07:45 +0200

    add new html

--- GlowHub/index.js
+++ GlowHub/index.js
@@ -2,3 +2,10 @@ document.querySelector('.button').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#background3').scrollIntoView({ behavior: 'smooth' });
         });
+
+function addToOrder(imageSrc, productName, productPrice) {
+    localStorage.setItem("productImage", imageSrc);
+    localStorage.setItem("productName", productName);
+    localStorage.setItem("productPrice", productPrice);
+    window.location.href = "orders.html";
+}
