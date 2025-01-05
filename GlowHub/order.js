464ccc687d85caf8092506d5580c14e3c479da42
Gergana Ivanova <geri7423@abv.bg>
2025-01-05 15:25:38 +0200

    update css

--- GlowHub/order.js
+++ GlowHub/order.js
@@ -1,16 +1,26 @@
-const productImage = localStorage.getItem("productImage");
-const productName = localStorage.getItem("productName");
-const productPrice = localStorage.getItem("productPrice");
-console.log("Данни от localStorage:");
-console.log("Снимка: " + productImage);
-console.log("Име: " + productName);
-console.log("Цена: " + productPrice);
+document.addEventListener('DOMContentLoaded', function() {
+    const productImage = localStorage.getItem("productImage");
+    const productName = localStorage.getItem("productName");
+    const productPrice = localStorage.getItem("productPrice");
+    const orderButton = document.getElementById("orderButton");
+    const orderDetails = document.getElementById("orderDetails");
+    const h1 = document.querySelector("h1");
 
-if (productImage && productName && productPrice) {
+    if (!productImage || !productName || !productPrice) {
+        orderDetails.innerHTML = "<p style='font-family: \"Bahnschrift Light\", sans-serif; font-weight: bold; font-size: 48px; margin-top: 200px; margin-bottom: 100px; text-align: center;'>Няма направени поръчки.</p>";
+        h1.style.display = "none"; 
+        orderButton.style.display = "none"; 
+    } else {
         document.getElementById("productImage").src = productImage;
         document.getElementById("productName").textContent = productName;
         document.getElementById("productPrice").textContent = productPrice;
-} else  {
-            document.getElementById("orderDetails").innerHTML = "<p>Няма избран продукт.</p>";
-        }
+    }
 
+    orderButton.addEventListener("click", function(event) {
+        event.preventDefault(); 
+
+        h1.style.display = "none";
+        orderButton.style.display = "none";
+        orderDetails.innerHTML = "<p style='font-family: \"Bahnschrift Light\", sans-serif; font-weight: bold; font-size: 48px; margin-top: 200px; margin-bottom: 100px; text-align: center;'>Благодарим за поръчката!</p>";
+    });
+});
