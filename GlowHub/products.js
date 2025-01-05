3ec0332b643b0a6714a932e702d92db6d099a461
Gergana Ivanova <geri7423@abv.bg>
2025-01-05 15:40:03 +0200

    update js

--- GlowHub/products.js
+++ GlowHub/products.js
@@ -12,3 +12,16 @@ for (let i = 1; i <= 12; i++) {
         });
 }
 
+
+function addToOrder(imageSrc, productName, productPrice) {
+    localStorage.setItem("productImage", imageSrc);
+    localStorage.setItem("productName", productName);
+    localStorage.setItem("productPrice", productPrice);
+
+    console.log("Данни за продукта:");
+    console.log("Снимка: " + localStorage.getItem("productImage"));
+    console.log("Име: " + localStorage.getItem("productName"));
+    console.log("Цена: " + localStorage.getItem("productPrice"));
+
+    window.location.href = "orders.html";
+}
