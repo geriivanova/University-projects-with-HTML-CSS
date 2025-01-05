129d36cd676a3ca08be1464b6dc58d046ac6b646
Gergana Ivanova <geri7423@abv.bg>
2025-01-05 13:05:52 +0200

    update css

--- GlowHub/gallery.js
+++ GlowHub/gallery.js
@@ -1,11 +1,26 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.gallery-item img');
+
 function openLightbox(img) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
     lightbox.style.display = 'block';
     lightboxImg.src = img.src;
+    currentImageIndex = Array.from(images).indexOf(img);
 }
 
 function closeLightbox() {
     const lightbox = document.getElementById('lightbox');
     lightbox.style.display = 'none';
+}
+
+function changeImage(direction) {
+    currentImageIndex += direction;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    } else if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = images[currentImageIndex].src;
 }
